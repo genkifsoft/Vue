bebe03136334d591cf71a560f69e17d4ff0095ba
Dang Quy <dangquy95php@gmail.com>
2020-03-23 22:09:35 +0700

    commmit code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import mixin from './library/mixin';
+import data from "../data.json";
+
+window.Vue = Vue;
+window.axios = require('axios');
+window.data = data;
+
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
+Vue.mixin(mixin);
+
 
 new Vue({
   el: '#app',
--- src/view/components/slidebar-left.vue
+++ src/view/components/slidebar-left.vue
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
         <img src="src/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Trang Quản Trị</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,7 +14,7 @@
                 <img src="src/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">Đặng Quý</a>
             </div>
         </div>
 
@@ -27,30 +27,9 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
-                            Dashboard
-                            <i class="right fas fa-angle-left"></i>
+                            Trang Chủ
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v2</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v3</p>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
                 <li class="nav-item">
                     <a href="../widgets.html" class="nav-link">
